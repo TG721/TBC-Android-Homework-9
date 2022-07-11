f395890073f679b2a7f68dd969d847ffcd7aea21
Tengiz <Tengiz152@gmail.com>
2022-07-11 08:51:42 +0400

    second commit

--- app/src/main/java/com/example/tbc_homework_9/UserAdapter.kt
+++ app/src/main/java/com/example/tbc_homework_9/UserAdapter.kt
@@ -1,13 +1,16 @@
 package com.example.tbc_homework_9
 
+import android.util.Log.d
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.example.tbc_homework_9.databinding.ItemUserBinding
 
 class UserAdapter(
     var users: List<User>
 ) : RecyclerView.Adapter<UserAdapter.UserViewHolder>() {
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UserViewHolder {
         val layoutInflater = LayoutInflater.from(parent.context)
         val binding = ItemUserBinding.inflate(layoutInflater, parent, false)
@@ -19,6 +22,10 @@ class UserAdapter(
             firstNameView.text = users[position].first_Name
             lastNameView.text = users[position].Last_name
             emailView.text = users[position].email
+
+            holder.binding.deleteButton.setOnClickListener {
+
+            }
         }
     }
 
@@ -26,6 +33,8 @@ class UserAdapter(
         return users.size
     }
 
-    inner class UserViewHolder(val binding: ItemUserBinding) : RecyclerView.ViewHolder(binding.root)
+    inner class UserViewHolder(val binding: ItemUserBinding) : RecyclerView.ViewHolder(binding.root){
+
+    }
 
 }
